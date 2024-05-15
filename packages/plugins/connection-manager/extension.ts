a412518b1cc11e17cc07ff66bf74b881d1e6a6d0
James Styman <35004248+Namyts@users.noreply.github.com>
2024-05-15 16:23:43 +0100

    fixed brackets

--- packages/plugins/connection-manager/extension.ts
+++ packages/plugins/connection-manager/extension.ts
@@ -168,7 +168,7 @@ export class ConnectionManagerPlugin implements IExtensionPlugin {
 
     if (Config.sessionFilesFolder && Config.sessionFilesFolder != '') {
       baseFolder = Uri.file(Config.sessionFilesFolder);
-      if(!path.isAbsolute(Config.sessionFilesFolder && workspace.workspaceFolders && workspace.workspaceFolders.length)){
+      if(!path.isAbsolute(Config.sessionFilesFolder) && workspace.workspaceFolders && workspace.workspaceFolders.length){
         baseFolder = path.resolve(workspace.workspaceFolders[0].uri, Config.sessionFilesFolder)
       }
     }
